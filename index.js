15cbfe35e0e1b420bcb33811ed8f933c4e37b070
Giovinko <keemrees@naver.com>
2025-04-01 11:38:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // ⭐ CORS 해결 핵심
+
   const { query } = req.query;
 
   if (!query) {
